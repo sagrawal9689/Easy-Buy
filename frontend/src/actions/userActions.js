f92701e64354c7c09aad8e76eea3991c7e43b827
unknown <sagrawal9689@gmail.com>
2021-07-10 01:39:18 -0700

    actions,reducers for user login created

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,28 @@
+import Axios from "axios"
+
+export const login= (email,password) = async(dispatch)=>{
+
+    try{
+        dispatch({ type: 'USER_LOGIN_REQUEST' })
+
+        const config={
+            headers:{
+                'Content-Type':'application/json'
+            }
+        }
+        
+        const { data }= await Axios.post('/api/users',{ email, password },config)
+
+        dispatch({ type: 'USER_LOGIN_SUCCESS' , payload: data })
+
+        localStorage.setItem('userInfo', JSON.stringify(data))
+    }
+    catch(err)
+    {
+        dispatch({
+            type: 'USER_LOGIN_FAIL',
+            payload: 
+            err.response && err.response.data.message?err.response.data.message: err.message
+        })
+    }
+}
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,18 +3,25 @@ import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReduer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
+import { userLoginReducer } from './reducers/userReducers'
 
 const reducer= combineReducers({
     productList: productListReduer,
     productDetails: productDetailsReducer,
-    cart: cartReducer 
+    cart: cartReducer,
+    userLogin: userLoginReducer 
 })
 
 const cartItemsFromStorage= localStorage.getItem('cartItems') ? (JSON.parse(localStorage.getItem('cartItems'))): []
+const userInfoFromStorage= localStorage.getItem('useInfo')? (JSON.parse(localStorage.getItem('userInfo'))): null
 
 const initialState= { cart: {
     cartItems: cartItemsFromStorage   
-} }
+},
+    userLogin:{
+        userInfo: userInfoFromStorage
+    }
+}
 
 const middleware= [thunk]
 
