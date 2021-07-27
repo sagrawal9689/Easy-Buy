751551330fcf04be18d7000a058ad5493321d93f
unknown <sagrawal9689@gmail.com>
2021-07-27 23:56:21 +0530

    added Delete product functionality in ProductListScreen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers , applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReduer, productDetailsReducer } from './reducers/productReducers'
+import { productListReduer, productDetailsReducer, productDeleteReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userDetailsReducer, userLoginReducer, userRegisterReducer } from './reducers/userReducers'
 import { orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayReducer } from './reducers/orderReducers'
@@ -16,7 +16,8 @@ const reducer= combineReducers({
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
-    orderListMy: orderListMyReducer 
+    orderListMy: orderListMyReducer,
+    productDelete: productDeleteReducer 
 })
 
 const cartItemsFromStorage= localStorage.getItem('cartItems') ? (JSON.parse(localStorage.getItem('cartItems'))): []
