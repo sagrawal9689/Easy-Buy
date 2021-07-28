59d77e29f2c8ad2be00d28734a4d3d2adb738c92
unknown <sagrawal9689@gmail.com>
2021-07-28 22:21:38 +0530

    CreateProduct Action and reducer created

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -73,3 +73,40 @@ export const deleteProduct = (id) => async (dispatch, getState) => {
       })
     }
   }
+
+export const createProduct = (productData) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: 'PRODUCT_CREATE_REQUEST',
+    })
+
+    const {
+      userLogin: { userInfo },
+    } = getState()
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    }
+
+    const { data } = await axios.post(`/api/products`, productData , config)
+
+    dispatch({
+      type: 'PRODUCT_CREATE_SUCCESS',
+      payload: data,
+    })
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message
+    if (message === 'Not authorized, token failed') {
+      dispatch(logout())
+    }
+    dispatch({
+      type: 'PRODUCT_CREATE_FAIL',
+      payload: message,
+    })
+  }
+}
