c3f26d02ff4f73c1b42cfe2bcb78a4e3ef8d6917
unknown <sagrawal9689@gmail.com>
2021-07-29 15:31:54 +0530

    Changed bg color of card

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,19 +4,19 @@ import { Card } from 'react-bootstrap'
 
 const Product = ({ product }) => {
   return (
-    <Card className='my-3 p-3 rounded' bg='light'>
+    <Card className='my-3 p-3 rounded' bg='dark'>
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant='top' />
       </Link>
 
       <Card.Body>
         <Link to={`/product/${product._id}`} style={{color: 'while'}}>
-          <Card.Title as='div'>
+          <Card.Title as='div' id='main-card-title'>
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
 
-        <Card.Text as='h3' style={{color: 'while'}}>Rs {product.price}</Card.Text>
+        <Card.Text as='h3' style={{color: 'white'}}>Rs {product.price}</Card.Text>
       </Card.Body>
     </Card>
   )
