e12ab38343d5b19f61b17e851eaa31b14a319722
unknown <sagrawal9689@gmail.com>
2021-08-07 00:29:36 +0530

    Image upoads to mongodb completed

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,41 +1,82 @@
 import path from 'path'
 import express from 'express'
 import multer from 'multer'
+import crypto from 'crypto'
+import mongoose from 'mongoose'
+import {GridFsStorage} from 'multer-gridfs-storage'
+import Grid from 'gridfs-stream'
+import dotenv from 'dotenv'
+
+dotenv.config({path: './config.env'})
+
+// Mongo URI
+const mongoURI = process.env.MONGO_URI;
+
+// console.log(mongoURI)
+
+// Create mongo connection
+const conn = mongoose.createConnection(mongoURI,
+  {useNewUrlParser: true, useUnifiedTopology: true});
+
+// Init gfs
+let gfs;
+
+conn.once('open', () => {
+  // Init stream
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: mongoURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = buf.toString('hex') + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: 'uploads'
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+const upload = multer({ storage });
+
 const router = express.Router()
 
-const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, 'uploads/')
-  },
-  filename(req, file, cb) {
-    cb(
-      null,
-      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
-    )
-  },
-})
-
-function checkFileType(file, cb) {
-  const filetypes = /jpg|jpeg|png/
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
-  const mimetype = filetypes.test(file.mimetype)
-
-  if (extname && mimetype) {
-    return cb(null, true)
-  } else {
-    cb('Images only!')
-  }
-}
-
-const upload = multer({
-  storage,
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb)
-  },
-})
-
 router.post('/', upload.single('image'), (req, res) => {
-  res.send(`/${req.file.path}`)
+
+  // console.log(req.file);
+
+  res.send(`/api/upload/image/${req.file.filename}`)
 })
+
+router.get('/image/:filename', (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: 'No file exists'
+      });
+    }
+
+    // Check if image
+    if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: 'Not an image'
+      });
+    }
+  });
+});
 
 export default router   
