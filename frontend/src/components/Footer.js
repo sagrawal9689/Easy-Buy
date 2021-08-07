ad526c5d75143b51c5b3979b0e892c0ca0e19278
unknown <sagrawal9689@gmail.com>
2021-08-07 11:25:12 +0530

    Footer and logo changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright &copy; EasyBuy</Col>
+          <Col className='text-center py-3'>Made by Sahil</Col>
         </Row>
       </Container>
     </footer>
